7123c538bb93149e5d7666f3b98c58af264b6b54
Max Hösel <git@maxhoesel.de>
2021-10-01 21:55:27 +0200

    feat(scan): improve scanning performance by conditionally skipping type determination
    Determinig the type of a file involves making a call to ffprobe.
    For larger libraries, this can incur a significant performance penalty on every scan.
    
    To get around this, this change moves the filetype determination in scan(), so
    that the type is only scanned for new or changed files. If the file has remained the same,
    we can just reuse the old filetype.

--- src/musicbird/scanner.py
+++ src/musicbird/scanner.py
@@ -45,7 +45,7 @@ class LibraryScanner:
         Returns:
             bool: True if all files were scanned successfully, False if not.
         """
-        successful = False
+        failed = False
 
         # Mark all files as deleted in-memory at the beginning of our scan
         # As files are rediscovered, the deleted flag will be removed
@@ -58,32 +58,37 @@ class LibraryScanner:
         for path in self.path.glob("./**/*"):
             if path.is_file() and path.name not in IGNORE_FILES:
                 file = File(path)
-                try:
-                    file.determine_type()
-                except ffmpeg.Error as e:
-                    logger.error(f"Could not determine filetype, skipping: {file.path}. FFmpeg error: {e.stderr}")
-                    successful = False
-                else:
-                    self.add_or_update_file(file)
-        successful = True
-        return successful
+                if not self.add_or_update_file(file):
+                    failed = True
 
-    def add_or_update_file(self, file: File) -> None:
+        return not failed
+
+    def add_or_update_file(self, file: File) -> bool:
         """Adds a single new file to the library or update an existing one.
 
         Args:
             file (File): The file object to add
+
+        Returns:
+            bool: True if the file was processed successfully, False if not
         """
         # If we're adding/updating a file, that means it must also exist physically
         file.was_deleted = False
 
         current_entry = self.db.get_file_by_path(file.path)
+
+        # By only scanning for the filetype after we detected a new/changed file, we can save a lot of calls
+        # to ffmpeg, thus considerably speeding up scanning. If the file is unchanged, we just reuse the old type.
         if not current_entry:
             logger.info(f"Adding new file to library: {file.path}")
+            file.determine_type()
             file.needs_processing = True
-        elif current_entry.mtime != file.mtime or current_entry.type != file.type:
+        elif current_entry.mtime != file.mtime:
             logger.info(f"Existing file has been modified and will be reprocessed: {file.path}")
+            file.determine_type()
             file.needs_processing = True
         else:
             logger.debug(f"File unchanged since last scan: {file.path}")
+            file.type = current_entry.type
         self.db.add_or_update_file(file)
+        return True
